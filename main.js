e11d0a7f3592ba5e9d89c3ac7f342eb44e018549
bivdub <bivdub@gmail.com>
2014-11-23 15:54:46 -0800

    remoes placeholders of cats and Nic Cage (sadly) for X and O, adds a toggle for playing against ai, changes how board layout is constructed so it doesn't break when adding content elements

--- main.js
+++ main.js
@@ -7,68 +7,60 @@ document.addEventListener('DOMContentLoaded', function() {
   var lastMove = [];
   var xArray = [];
   var oArray = [];
-  var aiPlay = true;
+  var aiPlay = false;
+
+ document.querySelector('#check').addEventListener('click', function () {
+  if (this.checked === true) {
+    aiPlay = true;
+  } else {
+    aiPlay = false;
+  }
+ })
 
   var reset = function() {
     for (var i = 0; i<tac.length; i++) {
       tac[i].classList.remove('O');
       tac[i].classList.remove('X');
-      tac[i].style.backgroundImage = null;   
     }
-    document.querySelector('.currentMove').innerText = "Cat's Move";
+    document.querySelector('.currentMove').innerText = "X's Move";
     turn = 1;
   }
 
   var aiMove = function(temp) {
-    // var temp = 0;
     for (var j = 0; j<8 && temp == 0; j++) {
       win1 = document.querySelector(winningCombos[j][0]);
       win2 = document.querySelector(winningCombos[j][1]);
       win3 = document.querySelector(winningCombos[j][2]);
       if (win1.classList.contains('O') && win2.classList.contains('O') && !win3.classList.contains('X')) {
-        win3.style.backgroundImage="url(http://placecage.com/c/80/75)";
         win3.classList.add('O');
-        document.querySelector('.currentMove').innerText = "Cat's Move";
-        // turn++;
+        document.querySelector('.currentMove').innerText = "X's Move";
         temp = 1;
       } else if (win1.classList.contains('O') && win3.classList.contains('O') && !win2.classList.contains('X')) {
-        win2.style.backgroundImage="url(http://placecage.com/c/80/75)";
         win2.classList.add('O');
-        document.querySelector('.currentMove').innerText = "Cat's Move";
-        // turn++;
+        document.querySelector('.currentMove').innerText = "X's Move";
         temp = 1;
       } else if (win2.classList.contains('O') && win3.classList.contains('O') && !win1.classList.contains('X')) {
-        win1.style.backgroundImage="url(http://placecage.com/c/80/75)";
         win1.classList.add('O');
-        document.querySelector('.currentMove').innerText = "Cat's Move";
-        // turn++;
+        document.querySelector('.currentMove').innerText = "X's Move";
         temp = 1;
       } else if (win1.classList.contains('X') && win2.classList.contains('X') && !win3.classList.contains('O')) {
-        win3.style.backgroundImage="url(http://placecage.com/c/80/75)";
         win3.classList.add('O');
-        document.querySelector('.currentMove').innerText = "Cat's Move";
-        // turn++;
+        document.querySelector('.currentMove').innerText = "X's Move";
         temp = 1;
       } else if (win1.classList.contains('X') && win3.classList.contains('X') && !win2.classList.contains('O')) {
-        win2.style.backgroundImage="url(http://placecage.com/c/80/75)";
         win2.classList.add('O');
-        document.querySelector('.currentMove').innerText = "Cat's Move";
-        // turn++;
+        document.querySelector('.currentMove').innerText = "X's Move";
         temp = 1;
       } else if (win2.classList.contains('X') && win3.classList.contains('X') && !win1.classList.contains('O')) {
-        win1.style.backgroundImage="url(http://placecage.com/c/80/75)";
         win1.classList.add('O');
-        document.querySelector('.currentMove').innerText = "Cat's Move";
-        // turn++;
+        document.querySelector('.currentMove').innerText = "X's Move";
         temp = 1;
       } 
     }    
     for (var k = 0; k<tac.length && temp == 0; k++) {
       if (tac[k].classList.contains('X') == false && tac[k].classList.contains('O') == false && temp == 0) {
-        tac[k].style.backgroundImage="url(http://placecage.com/c/80/75)";
         tac[k].classList.add('O');
-        document.querySelector('.currentMove').innerText = "Cat's Move";
-        // turn++;
+        document.querySelector('.currentMove').innerText = "X's Move";
         temp = 1;
       }
     }
@@ -81,18 +73,16 @@ document.addEventListener('DOMContentLoaded', function() {
       if (this.classList.contains('X') == false && this.classList.contains('O') == false) {
 
         if (turn % 2 === 1) {
-          this.style.backgroundImage="url(http://placekitten.com/g/80/75)";
           this.classList.add('X'); 
-          document.querySelector('.currentMove').innerText = "Cage's Move";
+          document.querySelector('.currentMove').innerText = "O's Move";
           turn++;
           if (aiPlay === true) {
             aiMove(0);
             turn++;
           }
         } else {
-          this.style.backgroundImage="url(http://placecage.com/c/80/75)";
           this.classList.add('O');
-          document.querySelector('.currentMove').innerText = "Cat's Move";
+          document.querySelector('.currentMove').innerText = "X's Move";
           turn++;
         }
         
@@ -107,10 +97,10 @@ document.addEventListener('DOMContentLoaded', function() {
       win3 = document.querySelector(winningCombos[j][2]);
 
       if ((win1.classList.contains('X')) && (win2.classList.contains('X')) && (win3.classList.contains('X'))) {
-        alert('Cats wins!');
+        alert('X wins!');
         reset();
       } else if ((win1.classList.contains('O')) && (win2.classList.contains('O')) && (win3.classList.contains('O'))) {
-        alert('Cage wins!');
+        alert('O wins!');
         reset();
       }
     }
